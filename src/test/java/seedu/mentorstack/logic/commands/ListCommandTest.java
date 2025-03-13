108d1d854c54085f898c9d2f0e224cbf367a1a9a
Zhang_Leran <A1908721346@gmail.com>
2025-03-13 15:31:36 +0800

    Update package name

---
+++ src/test/java/seedu/mentorstack/logic/commands/ListCommandTest.java
@@ -0,0 +1,39 @@
+package seedu.mentorstack.logic.commands;
+
+import static seedu.mentorstack.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.mentorstack.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.mentorstack.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.mentorstack.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.mentorstack.model.Model;
+import seedu.mentorstack.model.ModelManager;
+import seedu.mentorstack.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ListCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
