2f38e21d31d06d5efbfa64adbb895fc25c154be9
subiloble <e0773758@u.nus.edu>
2025-03-19 22:13:20 +0800

    Further bug fix for mentorstack data file path

--- src/test/java/seedu/mentorstack/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/mentorstack/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setMentorstackFilePath(Paths.get("data\\mentorstack.json"));
+        userPrefs.setMentorstackFilePath(Paths.get("mentorstack.json"));
         return userPrefs;
     }
 
