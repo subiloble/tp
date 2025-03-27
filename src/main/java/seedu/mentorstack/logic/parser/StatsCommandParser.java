89202c11701468324768aaf744612e53ffc57abb
SheYuting <e1032467@u.nus.edu>
2025-03-27 15:29:49 +0800

    Update mistake report

--- src/main/java/seedu/mentorstack/logic/parser/StatsCommandParser.java
+++ src/main/java/seedu/mentorstack/logic/parser/StatsCommandParser.java
@@ -1,11 +1,14 @@
 package seedu.mentorstack.logic.parser;
 
 import static seedu.mentorstack.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.mentorstack.logic.parser.CliSyntax.PREFIX_SUBJECT;
+import static seedu.mentorstack.logic.parser.CliSyntax.*;
 
+import seedu.mentorstack.logic.commands.AddCommand;
 import seedu.mentorstack.logic.commands.StatsCommand;
 import seedu.mentorstack.logic.parser.exceptions.ParseException;
 import seedu.mentorstack.model.person.Subject;
+
+import java.util.stream.Stream;
 
 /**
  * Parses the given {@code String} of arguments in the context of the StatsCommand
@@ -24,15 +27,23 @@ public class StatsCommandParser implements Parser<StatsCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_SUBJECT);
 
-        Subject subject;
-        try {
-            subject = ParserUtil.parseSubjects(argMultimap.getValue(PREFIX_SUBJECT).get());
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, StatsCommand.MESSAGE_USAGE));
+        if (!arePrefixesPresent(argMultimap, PREFIX_SUBJECT)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, StatsCommand.MESSAGE_USAGE));
         }
+        Subject subject;
+        subject = ParserUtil.parseSubjects(argMultimap.getValue(PREFIX_SUBJECT).get());
+
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_SUBJECT);
 
         return new StatsCommand(subject);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
