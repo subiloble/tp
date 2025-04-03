669dd8ef52858b377d465256f2df4f88be62b3db
SheYuting <e1032467@u.nus.edu>
2025-04-03 17:36:33 +0800

    Update add command with logging

--- src/main/java/seedu/mentorstack/logic/commands/AddCommand.java
+++ src/main/java/seedu/mentorstack/logic/commands/AddCommand.java
@@ -7,6 +7,9 @@ import static seedu.mentorstack.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.mentorstack.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.mentorstack.logic.parser.CliSyntax.PREFIX_SUBJECT;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import seedu.mentorstack.commons.util.ToStringBuilder;
 import seedu.mentorstack.logic.Messages;
 import seedu.mentorstack.logic.commands.exceptions.CommandException;
@@ -23,7 +26,7 @@ public class AddCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a student to Mentorstack. "
             + "Parameters: "
             + PREFIX_NAME + "NAME "
-            + PREFIX_GENDER + "GENDER"
+            + PREFIX_GENDER + "GENDER "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_SUBJECT + "SUBJECT...\n"
@@ -38,6 +41,8 @@ public class AddCommand extends Command {
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
 
+    private static final Logger logger = Logger.getLogger(AddCommand.class.getName());
+
     private final Person toAdd;
 
     /**
@@ -46,18 +51,23 @@ public class AddCommand extends Command {
     public AddCommand(Person person) {
         requireNonNull(person);
         toAdd = person;
+        logger.log(Level.INFO, "AddCommand created with person: {0}", person);
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        logger.log(Level.INFO, "Executing AddCommand for person: {0}", toAdd);
 
         if (model.hasPerson(toAdd)) {
+            logger.log(Level.WARNING, "Duplicate person detected: {0}", toAdd);
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
 
         model.rememberMentorstack(); // save the state for undo
         model.addPerson(toAdd);
+        logger.log(Level.INFO, "Person successfully added: {0}", toAdd);
+
         return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
     }
 
